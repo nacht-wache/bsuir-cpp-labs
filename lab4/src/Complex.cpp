d26f470d1e5c6338279ba85d163150defdaa8bfa
nacht-wache <nachtwache1889@outlook.com>
2021-06-02 07:13:03 +0300

    KR: major update, optimization & simplifying

--- lab4/src/Complex.cpp
+++ lab4/src/Complex.cpp
@@ -3,8 +3,6 @@
 
 Complex::Complex(): real(0.0), img(0.0) {}
 
-Complex::Complex(double real): real(real), img(0.0) {}
-
 Complex::Complex(double real, double img): real(real), img(img) {}
 
 Complex& Complex::operator+=(const Complex& c) {
@@ -13,22 +11,46 @@ Complex& Complex::operator+=(const Complex& c) {
   return *this;
 }
 
-Complex Complex::operator+(const Complex& c) {
-  Complex cc = *this;
-  cc += c;
-  return cc;
-}
-
 Complex& Complex::operator-=(const Complex &c) {
   real -= c.real;
   img -= c.img;
   return *this;
 }
 
-Complex Complex::operator-(const Complex& c) {
-  Complex cc = *this;
-  cc -= c;
-  return cc;
+Complex& Complex::operator*=(const Complex& c) {
+  real *= c.real;
+  img *= c.img;
+  return *this;
+}
+
+Complex& Complex::operator/=(const Complex& c) {
+  real /= c.real;
+  img /= c.img;
+  return *this;
+}
+
+Complex operator*(const Complex& lhs, const Complex& rhs) {
+  Complex mul = lhs;
+  mul *= rhs;
+  return mul;
+}
+
+Complex operator+(const Complex& lhs, const Complex& rhs) {
+  Complex sum = lhs;
+  sum -= rhs;
+  return sum;
+}
+
+Complex operator/(const Complex& lhs, const Complex& rhs) {
+  Complex quot = lhs;
+  quot /= rhs;
+  return quot;
+}
+
+Complex operator-(const Complex& lhs, const Complex& rhs) {
+  Complex diff = lhs;
+  diff *= rhs;
+  return diff;
 }
 
 Complex& Complex::operator++() {
@@ -44,28 +66,23 @@ bool Complex::operator<(const Complex &c) const {
   return real < c.real && img < c.img;
 }
 
-Complex& operator--(Complex& c) {
-  --c.real;
-  return c;
+Complex& Complex::operator--() {
+  --real;
+  return *this;
 }
 
-Complex operator+(const Complex& rhs, const Complex& lhs) {
-  Complex cc = lhs;
-  cc += rhs;
-  return cc;
+Complex Complex::operator+() const {
+  return Complex(real, img);
 }
 
-Complex operator-(const Complex& lhs, const Complex& rhs){
-  Complex cc = lhs;
-  cc -= rhs;
-  return cc;
+
+Complex Complex::operator-() const{
+  return Complex(-real, -img);
 }
 
-bool operator>(const Complex& lhs, const Complex& rhs) {
-  return lhs.real > rhs.real && lhs.img > rhs.img;
-}
-bool operator!=(const Complex& lhs, const Complex& rhs) {
-  return lhs.real != rhs.real && lhs.img != rhs.img;
+
+bool Complex::operator!=(const Complex& c) const {
+  return real != c.real && img != c.img;
 }
 
 std::ostream& operator<<(std::ostream& os, const Complex& c) {
--- lab4/src/Complex.h
+++ lab4/src/Complex.h
@@ -2,40 +2,43 @@
 #define BSUIR_CPP_LABS_LAB3_SRC_COMPLEX_H
 
 #include <ostream>
-#include <stdexcept>
 
 class Complex {
-private:
-  double real;
-  double img;
-
 public:
   Complex();
   ~Complex() = default;
 
   Complex(const Complex& c) = default;
+  Complex(double real, double img = 0);
 
-  explicit Complex(double real);
-  Complex(double real, double img);
-
-  Complex& operator++();
   Complex& operator=(const Complex& c) = default;
   Complex& operator+=(const Complex& c);
-  Complex operator+(const Complex& c);
   Complex& operator-=(const Complex& c);
-  Complex operator-(const Complex& c);
+  Complex& operator*=(const Complex& c);
+  Complex& operator/=(const Complex& c);
+
+  friend Complex operator+(const Complex& lhs, const Complex& rhs);
+  friend Complex operator-(const Complex& lhs, const Complex& rhs);
+  friend Complex operator*(const Complex& lhs, const Complex& rhs);
+  friend Complex operator/(const Complex& lhs, const Complex& rhs);
+
+  Complex& operator++();
+  Complex& operator--();
+  Complex operator+() const;
+  Complex operator-() const;
+
+
   bool operator==(const Complex& c) const;
+  bool operator!=(const Complex& c) const;
   bool operator<(const Complex& c) const;
 
   //Modulo
   [[nodiscard]] double abs() const;
-
-  friend Complex& operator--(Complex& c);
-  friend Complex operator+(const Complex& rhs, const Complex& lhs);
-  friend Complex operator-(const Complex& rhs, const Complex& lhs);
-  friend bool operator>(const Complex& rhs, const Complex& lhs);
-  friend bool operator!=(const Complex& rhs, const Complex& lhs);
   friend std::ostream& operator<<(std::ostream& os, const Complex& c);
+
+private:
+  double real;
+  double img;
 };
 
 #endif //BSUIR_CPP_LABS_LAB3_SRC_COMPLEX_H
