431eab553a0799798f0d5e3197076c1f18caebd9
nacht-wache <nachtwache1889@outlook.com>
2021-06-30 01:51:30 +0300

    KR: solution is provided, typedefs are implemented

--- lab4_dynamic_memory_allocation/12_src/Array.h
+++ lab4_dynamic_memory_allocation/12_src/Array.h
@@ -6,145 +6,217 @@
 
 template <typename T, typename Alloc = std::allocator<T>>
 class Array {
- private:
-  T* arr = nullptr;
-  size_t sz = 0;
-  size_t cap = 0;
-  Alloc alloc;
-
-  Array(T* arr, size_t sz, size_t cap)
-	  : arr(arr), sz(sz), cap(cap) {}
-
  public:
+
+  typedef T value_type;
+  typedef Alloc allocator_type;
+  typedef size_t size_type;
+  typedef value_type& reference;
+  typedef const value_type& const_reference;
+  typedef value_type&& rvalue_reference;
+  typedef const value_type&& const_rvalue_reference;
+  typedef typename std::allocator_traits<allocator_type>::pointer pointer;
+  typedef typename std::allocator_traits<allocator_type>::const_pointer const_pointer;
+
   Array() = default;
-  Array(const Array& rhs);
-  Array(Array&& rhs) noexcept;
+  Array(const Array& other);
+  Array(Array&& other) noexcept;
   ~Array() noexcept;
 
-  Array& operator=(const Array& rhs);
-  Array& operator=(Array&& rhs) noexcept;
+  Array& operator=(const Array& other);
+  Array& operator=(Array&& other) noexcept;
+  reference operator[](size_type pos);
+  const_reference operator[](size_type pos) const;
 
-  void reserve(size_t n);
-  void resize(size_t n);
-  void push_back(const T& value);
-  void push_back(T&& value) noexcept;
+
+  void reserve(size_type count);
+  void resize(size_type count);
+  void push_back(const_reference value);
+  void push_back(rvalue_reference value) noexcept;
 
   //helper func
   void sort();
-  [[nodiscard]] size_t size() const { return sz; }
-  [[nodiscard]] size_t capacity() const { return cap; }
+  [[nodiscard]] size_type size() const { return sz; }
+  [[nodiscard]] size_type capacity() const { return cap; }
+
+  static void* operator new(size_t count) {
+    std::cout << "allocated " << count << " bytes\n";
+	return ::operator new(count);
+  }
+
+  static void* operator new(size_t, void* ptr) {
+	std::cout << "placement new\n";
+	return ptr;
+  }
+
+  static void* operator new[](size_t count) {
+	std::cout << "allocated[] " << count << " bytes\n";
+	return ::operator new(count);
+  }
+
+  static void* operator new[](size_t, void* ptr) {
+	std::cout << "placement new\n";
+	return ptr;
+  }
+
+  void operator delete(void* ptr) {
+    std::cout << "deallocated\n";
+    ::operator delete(ptr);
+  }
+
+  void operator delete[](void* ptr) {
+	std::cout << "deallocated[]\n";
+	::operator delete(ptr);
+  }
+
+
+ private:
+  pointer arr = nullptr;
+  size_type sz = 0;
+  size_type cap = 0;
+  allocator_type alloc;
+
+  Array(pointer arr, size_type sz, size_type cap)
+	  : arr(arr), sz(sz), cap(cap) {};
 };
 
-template <typename T, typename Alloc>
-void Array<T, Alloc>::push_back(const T& value) {
-  if (cap == 0) { reserve(1); }
-  if (sz == cap) { reserve(2 * sz); }
+template <typename value_type, typename allocator_type>
+void Array<value_type, allocator_type>::push_back(const_reference value) {
+  if (capacity() == 0) { reserve(1); }
+  if (size() == capacity()) { reserve(2 * sz); }
 
-  new(arr + sz) T(value);
+  new(arr + sz) value_type(value);
   ++sz;
 }
 
-template <typename T, typename Alloc>
-void Array<T, Alloc>::push_back(T&& value) noexcept {
-  if (cap == 0) { reserve(1); }
-  if (sz == cap) { reserve(2 * sz); }
+template <typename value_type, typename allocator_type>
+void Array<value_type, allocator_type>::push_back(rvalue_reference value) noexcept {
+  if (capacity() == 0) { reserve(1); }
+  if (size() == capacity()) { reserve(2 * sz); }
 
-  new(arr + sz) T(std::move_if_noexcept(value));
+  new(arr + sz) value_type(std::move_if_noexcept(value));
   ++sz;
 }
 
-template <typename T, typename Alloc>
-Array<T, Alloc>::~Array() noexcept {
+template <typename value_type, typename allocator_type>
+Array<value_type, allocator_type>::~Array() noexcept {
 
-  for (size_t i = 0; i < sz; ++i) {
-	std::allocator_traits<Alloc>::destroy(alloc, arr + sz - 1 - i);
+  for (size_type i = 0; i < sz; ++i) {
+	std::allocator_traits<allocator_type>::destroy(alloc, arr + sz - 1 - i);
   }
 
   ::operator delete(static_cast<void*>(arr), cap);
 }
 
-template <typename T, typename Alloc>
-Array<T, Alloc>::Array(const Array& rhs)
-	: Array(static_cast<T*>(::operator new(rhs.cap * sizeof(T))), rhs.sz, rhs.cap) {
+template <typename value_type, typename allocator_type>
+Array<value_type, allocator_type>::Array(const Array& other)
+	: Array(static_cast<pointer>(::operator new(other.cap * sizeof(value_type))), other.sz, other.cap) {
   try {
-	std::uninitialized_copy(rhs.arr, rhs.arr + sz, arr);
+	std::uninitialized_copy(other.arr, other.arr + sz, arr);
   } catch (...) {
 	::operator delete(static_cast<void*>(arr));
 	throw;
   }
 }
 
-template <typename T, typename Alloc>
-Array<T, Alloc>::Array(Array&& rhs) noexcept
-	: Array(rhs.arr, rhs.sz, rhs.cap) {
-  rhs.arr = nullptr;
-  rhs.sz = 0;
-  rhs.cap = 0;
+template <typename value_type, typename allocator_type>
+Array<value_type, allocator_type>::Array(Array&& other) noexcept
+	: Array(other.arr, other.sz, other.cap) {
+  other.arr = nullptr;
+  other.sz = 0;
+  other.cap = 0;
 
 }
 
-template <typename T, typename Alloc>
-void Array<T, Alloc>::reserve(size_t n) {
-  if (n <= cap) { return; }
+template <typename value_type, typename allocator_type>
+void Array<value_type, allocator_type>::reserve(size_type count) {
+  if (count <= cap) { return; }
 
-  T* newarr = static_cast<T*>(::operator new(n * sizeof(T)));
-  size_t i = 0;
+  auto newarr = static_cast<pointer>(::operator new(count * sizeof(value_type)));
+  size_type i = 0;
   try {
 	for (; i < sz; ++i) {
-	  new(newarr + i) T(std::move_if_noexcept(arr[i]));
+	  new(newarr + i) value_type(std::move_if_noexcept(arr[i]));
 	}
   } catch (...) {
-	for (size_t j = 0; j < i; ++j) {
-	  std::allocator_traits<Alloc>::destroy(alloc, newarr + i - j);
+	for (size_type j = 0; j < i; ++j) {
+	  std::allocator_traits<allocator_type>::destroy(alloc, newarr + i - j);
 	}
 	::operator delete(static_cast<void*>(newarr));
 	throw;
   }
 
-  for (size_t i = 0; i < sz; ++i) {
-	std::allocator_traits<Alloc>::destroy(alloc, arr + sz - 1 - i);
+  for (size_type k = 0; k < sz; ++k) {
+	std::allocator_traits<allocator_type>::destroy(alloc, arr + sz - 1 - k);
   }
 
   ::operator delete(static_cast<void*>(arr), cap);
   arr = newarr;
-  cap = n;
+  cap = count;
 }
 
-template <typename T, typename Alloc>
-Array<T, Alloc>& Array<T, Alloc>::operator=(Array&& rhs) noexcept {
+template <typename value_type, typename allocator_type>
+Array<value_type, allocator_type>& Array<value_type, allocator_type>::operator=(Array&& other) noexcept {
   //Handle self-assignment idiom
-  if (this == &rhs) {
+  if (this == &other) {
 	return *this;
   }
 
-  Array<T, Alloc> copy(std::move_if_noexcept(*this));
-  arr = rhs.arr;
-  sz = rhs.sz;
-  cap = rhs.cap;
+  Array<value_type, allocator_type> copy(std::move_if_noexcept(*this));
+  arr = other.arr;
+  sz = other.sz;
+  cap = other.cap;
 
-  rhs.arr = nullptr;
-  rhs.sz = 0;
-  rhs.cap = 0;
+  other.arr = nullptr;
+  other.sz = 0;
+  other.cap = 0;
 }
 
-template <typename T, typename Alloc>
-Array<T, Alloc>& Array<T, Alloc>::operator=(const Array<T, Alloc>& rhs) {
+template <typename value_type, typename allocator_type>
+Array<value_type, allocator_type>& Array<value_type, allocator_type>::operator=(const Array<value_type,
+																							allocator_type>& other) {
   //Handle self-assignment idiom
-  if (this == &rhs) {
+  if (this == &other) {
 	return *this;
   }
 
-  Array<T, Alloc> copy(std::move_if_noexcept(*this));
-  arr = static_cast<T*>(::operator new(rhs.cap * sizeof(T)));
-  size_t i = 0;
+  Array<value_type, allocator_type> copy(std::move_if_noexcept(*this));
+  arr = static_cast<pointer>(::operator new(other.cap * sizeof(value_type)));
   try {
-    std::uninitialized_copy(rhs.arr, rhs.arr + sz, arr);
+	std::uninitialized_copy(other.arr, other.arr + other.sz, arr);
   } catch (...) {
 	::operator delete(static_cast<void*>(arr));
 	throw;
   }
 
-  sz = rhs.sz;
-  cap = rhs.cap;
+  sz = other.sz;
+  cap = other.cap;
+}
+
+template <typename value_type, typename allocator_type>
+void Array<value_type, allocator_type>::resize(size_type count) {
+  if (count == size()) {
+	return;
+  }
+  if (count > size()) {
+	reserve(count);
+	for (size_type i = sz; i < count; ++i) {
+	  std::allocator_traits<allocator_type>::construct(alloc, arr + i, value_type());
+	}
+  }
+  if (count < size()) {
+    sz = count;
+    // ... some magic
+  }
+}
+
+template <typename T, typename Alloc>
+typename Array<T, Alloc>::reference Array<T, Alloc>::operator[](Array::size_type pos) {
+  return *(arr+pos);
+}
+
+template <typename T, typename Alloc>
+typename Array<T, Alloc>::const_reference Array<T, Alloc>::operator[](Array::size_type pos) const {
+  return *(arr+pos);
 }
 #endif //BSUIR_CPP_LABS_LAB4_SRC_ARRAY3D_COMPLEX_H
--- lab4_dynamic_memory_allocation/12_src/main.cpp
+++ lab4_dynamic_memory_allocation/12_src/main.cpp
@@ -2,30 +2,73 @@
 #include <vector>
 #include <Array.h>
 
+void print(Array<Array<Array<Complex>>>& arr3D) {
+  for(size_t i = 0; i < arr3D.size(); ++i) {
+	for(size_t j = 0; j < arr3D[i].size(); ++j) {
+	  for(size_t k = 0; k < arr3D[i][j].size(); ++k) {
+		std::cout << arr3D[i][j][k] << ' ';
+	  }
+	  std::cout << '\n';
+	}
+	std::cout << '\n';
+  }
+}
+
+void heap_obj(Array<Array<Array<Complex>>>& arr3D) {
+  auto* new_arr3D = new Array<Array<Array<Complex>>>(arr3D);
+  auto new_arr3D_copy = new Array<Array<Array<Complex>>>(*new_arr3D);
+
+  for(size_t i = 0; i < new_arr3D->size(); ++i) {
+	for(size_t j = 0; j < (*new_arr3D)[i].size(); ++j) {
+	  for(size_t k = 0; k < (*new_arr3D)[i][j].size(); ++k) {
+		(*new_arr3D)[i][j][k] = Complex(.5, .7);
+	  }
+	}
+  }
+
+  print(*new_arr3D);
+  print(*new_arr3D_copy);
+
+  delete new_arr3D;
+  delete new_arr3D_copy;
+}
+
+void heap_arr(Array<Array<Array<Complex>>>& arr3D) {
+  auto new_arr_of_arr3D = new Array<Array<Array<Complex>>>[4];
+  for(size_t i = 0; i < 4; ++i) {
+	new_arr_of_arr3D[i] = arr3D;
+  }
+
+  for(size_t i = 0; i < new_arr_of_arr3D->size(); ++i) {
+	for(size_t j = 0; j < (*new_arr_of_arr3D)[i].size(); ++j) {
+	  for(size_t k = 0; k < (*new_arr_of_arr3D)[i][j].size(); ++k) {
+		(*new_arr_of_arr3D)[i][j][k] = Complex(.5, .7);
+	  }
+	}
+  }
+
+  for(size_t i = 0; i < 4; ++i) {
+	print(*(new_arr_of_arr3D+i));
+  }
+
+  delete[] new_arr_of_arr3D;
+}
+
 int main() {
-  Array<Array<Array<Complex>>> arr;
-  Array<Array<Complex>> arr_1;
-  Array<Complex> arr_2;
+  Array<Array<Array<Complex>>> arr3D;
+  Array<Array<Complex>> arr2D;
+  Array<Complex> arr1D;
 
-  for(int i = 0; i < 1; ++i) {
-    arr_2.push_back(Complex());
-  }
-  for(int i = 0; i < 1; ++i) {
-	arr_1.push_back(arr_2);
-  }
-  for(int i = 0; i < 1; ++i) {
-	arr.push_back(arr_1);
-  }
-
-  Array<Complex> array;
   for(int i = 0; i < 2; ++i) {
-	array.push_back(Complex());
+    arr1D.push_back(Complex(.3, .4));
+  }
+  for(int i = 0; i < 2; ++i) {
+	arr2D.push_back(arr1D);
+  }
+  for(int i = 0; i < 2; ++i) {
+	arr3D.push_back(arr2D);
   }
 
-  Array<Complex> array_copy;
-  for(int i = 0; i < 2; ++i) {
-	array_copy.push_back(Complex());
-  }
-  array = array_copy;
-  array_copy.push_back(Complex());
+  heap_obj(arr3D);
+  heap_arr(arr3D);
 }
